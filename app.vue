875d411b618d1d7ebf8d74f8f6606c322c13453e
darshitdudhaiya <darshitdudhaiya201@gmail.com>
2024-11-27 04:13:24 +0530

    𝟯𝗗 𝗧𝗲𝘅𝘁 𝗘𝗳𝗳𝗲𝗰𝘁 Demo with 𝗡𝘂𝘅𝘁 𝗮𝗻𝗱 𝗜𝗻𝘀𝗽𝗶𝗿𝗮 𝗨𝗜 🎨✨

--- app.vue
+++ app.vue
@@ -1,7 +1,9 @@
 <template>
-  <Card />
+  <!-- <Card /> -->
+  <Text />
 </template>
 
 <script setup>
 import Card  from "./pages/Card";
+import Text  from "./pages/Text";
 </script>
---
+++ pages/Text.vue
@@ -0,0 +1,25 @@
+<template>
+  <ClientOnly>
+    <div
+      class="flex items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900"
+    >
+      <div>
+        <Text3d class="my-5 text-8xl font-bold max-md:text-7xl" shadow-color="red">
+          3D
+        </Text3d>
+        <Text3d
+          class="my-5 text-8xl font-black max-md:text-7xl"
+          shadow-color="skyblue"
+          :animate="false"
+        >
+          IS
+        </Text3d>
+        <Text3d class="my-5 text-8xl font-bold max-md:text-7xl">AWESOME</Text3d>
+      </div>
+    </div>
+  </ClientOnly>
+</template>
+
+<script setup>
+import Text3d from "../components/Text/3DText.vue";
+</script>
