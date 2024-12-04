c278260f027c06e9ea051e0011c2d37a0001c18f
darshitdudhaiya <darshitdudhaiya201@gmail.com>
2024-12-05 01:35:27 +0530

    𝗦𝗹𝗶𝗱𝗲 𝘁𝗼 𝗖𝗼𝗺𝗽𝗮𝗿𝗲

---
+++ pages/DragHandleCompare.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex w-full justify-center">
+    <Compare slide-mode="drag" class="rounded-xl !bg-zinc-950">
+      <template #first-content>
+        <div class="size-full overflow-auto bg-zinc-900 p-8">
+          <pre class="font-mono text-red-400">{{ firstCode }}</pre>
+        </div>
+      </template>
+      <template #second-content>
+        <div class="size-full overflow-auto bg-zinc-900 p-8">
+          <pre class="font-mono text-green-400">{{ secondCode }}</pre>
+        </div>
+      </template>
+    </Compare>
+  </div>
+</template>
+
+<script setup lang="ts">
+const firstCode = `function greet() {
+    console.log("Hello, World!");
+  }`;
+
+const secondCode = `function greet() {
+    console.log("Hello, World!");
+  }`;
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex">
+    <!-- <Card /> -->
+    <!-- <Text /> -->
+    <!-- <Grid /> -->
+    <!-- <Progressbar/> -->
+    <!-- <List/> -->
+    <!-- <Slider/> -->
+    <!-- <BlurReveal /> -->
+    <!-- <BoxReveal /> -->
+    <AutoplayCompare />
+  </div>
+</template>
+
+<script setup>
+import Card from "./Card";
+import Text from "./Text";
+import Grid from "./Grid";
+import Beam from "./Beam";
+import Progressbar from "./Progress-bar";
+import List from "./List";
+import Avatar from "./Avatar";
+import Slider from "./Slider";
+import BlurReveal from "./Blur_Reveal.vue";
+import BoxReveal from "./BoxReveal.vue";
+import AutoplayCompare from "./AutoplayCompare.vue";
+</script>
